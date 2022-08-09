9c243c70eae9f779122f5d7a52d54a02cde6edb0
Ricardo Reis <ricardo.luis.reis@gmail.com>
2022-08-09 16:58:55 -0300

    Update installandronode.sh

--- installandronode.sh
+++ installandronode.sh
@@ -43,7 +43,7 @@ EOF
 }
 
 create_ssh_command(){
-    cat > sshcommand.sh <<'EOF'
+    cat > $TARGET_DIR/sshcommand.sh <<'EOF'
 #!/bin/bash
 USER=$(whoami)
 IP=$(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1)
