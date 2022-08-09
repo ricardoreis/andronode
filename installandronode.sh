54ca48b5d1a88928bbe64e63909bde6dc3a1bbf7
Ricardo Reis <ricardo.luis.reis@gmail.com>
2022-08-09 16:14:43 -0300

    Add file via upload

---
+++ installandronode.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# START COMMAND
+# curl https://installnode.sh | bash
+
+TARGET_DIR=$HOME/andronode
+
+# Upgrade Termux Packages 
+pkg upgrade -y
+
+# Instal Termux Packages 
+pkg install termux-services -y
+pkg install bitcoin -y
+
+# Termux Setup Storage
+
+
+# Create Folders
+create_target_dir() {
+    rm -rf $TARGET_DIR 
+    mkdir $TARGET_DIR
+    mkdir $TARGET_DIR/webserver
+}
+ 
+# SSH
+# Create SSH files
+create_ssh_setup(){
+    cat > $TARGET_DIR/sshsetup.sh <<EOF
+#!/bin/sh
+pkg install openssh -y
+pkg install iproute2 -y
+sshd
+clear
+echo ""
+echo "Please create ssh Password"
+echo "--------------------------"
+passwd
+EOF
+    chmod +x $TARGET_DIR/sshsetup.sh
+
+    echo "sshsetup.sh has been created."
+}
+
+create_ssh_command(){
+    cat > $TARGET_DIR/sshcommand.sh <<EOF
+#!/bin/bash
+USER=$(whoami)
+IP=$(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1)
+echo ""
+echo "COMMAND:"
+echo ""
+echo "     ssh -p 8022 $USER@$IP"
+echo ""
+echo ""
+EOF
+    chmod +x $TARGET_DIR/sshcommand.sh
+    
+    echo "sshcommand.sh has been created."
+}
+
+
+
+# Create config.json
